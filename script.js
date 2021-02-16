49f3cd9cd8107b5924d53c656bc8e7b561bbc371
Chris Martinez <cbmartinez42@gmail.com>
2021-02-16 15:49:29 -0600

    adding some html and css elements as well as with some js added

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// link charAmount slider and input box
+const charAmtRange = document.getElementById
+('charAmtRange')
+const charAmtNumber = document.getElementById
+('charAmtNumber')
+
+// set const for checkbox values
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
+const includeNumbersElement = document.getElementById
+('includeNumbers')
+const includeSpecialCharElement = document.getElementById
+('includeSpecialChar')
+
+charAmtNumber.addEventListener('input', syncCharacterAmount)
+charAmtRange.addEventListener('input', syncCharacterAmount)
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  charAmtNumber.value = value
+  charAmtRange.value = value
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +35,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  const charAmtNumber = charAmtNumber.value
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSpecialChar)
+
