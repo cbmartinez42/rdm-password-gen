107cae4ccab3def9331c79e13dcd0bb7394b62b9
Chris Martinez <cbmartinez42@gmail.com>
2021-02-18 13:20:22 -0600

    updating with working prompts

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// variables for password characters
+// global variables for password characters
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -12,18 +12,23 @@ var symbols = ['!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', 
 // Write password to the #password input
 function writePassword() {
   // user must choose length (8-128 characters)
-var passLength = prompt("How long would you like the password to be? (choose between 8-128");
-// validate if it's a number
+var passLength = prompt("How long would you like the password to be? (choose between 8-128)");
+// validate that a entry is made and whether it's a number
 // validate if it's between 8 and 128
-    if (isNaN(passLength) !== false) {
-      alert("Password length must be a NUMBER between 8 and 128. Please try again");
+    while ((passLength = "") === true) {
+      alert('Please enter a number between 8 and 128');
       var passLength = prompt("How long would you like the password to be? (choose between 8-128)");
-    } else if (enter < "8" || enter > "128") {
-      alert("Password length must be a number BETWEEN 8 and 128. Please try again");
+    } 
+    passLength.isInteger; {
+      alert("Hey now - Password length must be a NUMBER between 8 and 128. Please try again");
       var passLength = prompt("How long would you like the password to be? (choose between 8-128)");
-    } else {
+    } 
+    while (passLength < 8 || passLength > 128) {
+      alert("Don't forget - Password length must be a number BETWEEN 8 and 128. Please try again");
+      var passLength = prompt("How long would you like the password to be? (choose between 8-128)");
+    } 
       alert("Your password will have " + passLength + " characters")
-    }
+    
 
 
 // user must choose if they want uppercase
